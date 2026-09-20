76c57d1f16e741368b4726cb065255f721e83076
radhekant230-debug <radhekant230@gmail.com>
2026-09-20 10:33:40 +0530

    Update Actions.js

--- client/src/Actions.js
+++ client/src/Actions.js
@@ -1,10 +1,8 @@
-// All the events
-
 export const ACTIONS = {
   JOIN: "join",
   JOINED: "joined",
   DISCONNECTED: "disconnected",
-  CODE_CHANGE: "conde-change",
+  CODE_CHANGE: "code-change",
   SYNC_CODE: "sync-code",
   LEAVE: "leave",
 };
