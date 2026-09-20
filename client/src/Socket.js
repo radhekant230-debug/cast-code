52aeb93abad25e214a2559a54c9179bf0a292cb6
radhekant230-debug <radhekant230@gmail.com>
2026-09-20 10:13:49 +0530

    Add transports option for Socket.io connection

--- client/src/Socket.js
+++ client/src/Socket.js
@@ -5,6 +5,7 @@ export const initSocket = async () => {
     forceNew: true,
     reconnectionAttempts: Infinity,
     timeout: 10000,
+    transports: ["websocket", "polling"],
   };
 
   return io(process.env.REACT_APP_BACKEND_URL, options);
